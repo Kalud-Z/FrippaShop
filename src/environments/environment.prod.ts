02b6dd6e7f52dd648ec94e7bffd885d7705a3751
Kalud.Z <khaled.fhm@live.fr>
2020-05-16 03:37:18 +0200

    app deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
-  fireBaseAPIKey : 'AIzaSyDIVA6P44Okinr0qT4z9XzJCpzX1qRDuwo'
+  
+  fireBaseAPIKey : 'AIzaSyDIVA6P44Okinr0qT4z9XzJCpzX1qRDuwo',
+  ahmedEmail : 'ahmed.fahem@kedgebs.com',
+  khaledEmail : 'khaled.fhm@live.fr',
+
+  ahmedName : 'ahmed',
+  khaledName : 'khaled'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,6 @@ export const environment = {
 
   ahmedName : 'ahmed',
   khaledName : 'khaled'
-
 };
 
 /*

0912fea39c5f33bf3eec989b68f8c47770b06d05
Kalud.Z <khaled.fhm@live.fr>
2020-05-29 03:46:37 +0200

    now gonna add images to tasks

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -13,8 +13,6 @@ export class AuthGuard implements CanActivate {  //#############################
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
      return this.authService.userSubject.pipe(map(user => {
-      //  console.log('im in guard')
-      //  console.log('this is the user' , user)
        if(!!user === true) {return true}
        else {
         return this.router.createUrlTree(['/login']);
